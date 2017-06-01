6ca35f17bbcc3c94e15d2d4794c5fd3d3c67575a
wscotten <wscotten@uci.edu>
2017-06-01 01:55:46 -0700

    3rd layer corners oriented correctly

--- python/complete_third_layer.py
+++ python/complete_third_layer.py
@@ -104,18 +104,119 @@ def match_back_edges_to_sides(cube, solution):
         if(check_back_edges_to_sides(cube, solution)):
             third_layer_algorithm_swap_edges(cube, solution)
 
+def third_layer_algorithm_position_corners(cube, solution):
+    # U R U'  L'  U R'  U'  L
+    # B R BBB LLL B RRR BBB L
+    rotate_back(cube, 1, solution)
+    rotate_right(cube, 1, solution)
+    rotate_back(cube, 3, solution)
+    rotate_left(cube, 3, solution)
+    rotate_back(cube, 1, solution)
+    rotate_right(cube, 3, solution)
+    rotate_back(cube, 3, solution)
+    rotate_left(cube, 1, solution)
+
+def position_back_corners_to_sides(cube, solution):
+    i = 0
+    while ( # top right
+            (
+            cube["top"][2] != cube["top"][4] and
+            cube["top"][2] != cube["back"][4] and
+            cube["top"][2] != cube["right"][4]
+            ) or
+            (
+            cube["right"][2] != cube["top"][4] and
+            cube["right"][2] != cube["back"][4] and
+            cube["right"][2] != cube["right"][4]
+            ) or
+            (
+            cube["back"][0] != cube["top"][4] and
+            cube["back"][0] != cube["back"][4] and
+            cube["back"][0] != cube["right"][4]
+            ) or
+            # top left
+            (
+            cube["top"][0] != cube["top"][4] and
+            cube["top"][0] != cube["back"][4] and
+            cube["top"][0] != cube["left"][4]
+            ) or
+            (
+            cube["left"][0] != cube["top"][4] and
+            cube["left"][0] != cube["back"][4] and
+            cube["left"][0] != cube["left"][4]
+            ) or
+            (
+            cube["back"][2] != cube["top"][4] and
+            cube["back"][2] != cube["back"][4] and
+            cube["back"][2] != cube["left"][4]
+            ) or
+            # bottom right
+            (
+            cube["bottom"][8] != cube["bottom"][4] and
+            cube["bottom"][8] != cube["back"][4] and
+            cube["bottom"][8] != cube["right"][4]
+            ) or
+            (
+            cube["right"][8] != cube["bottom"][4] and
+            cube["right"][8] != cube["back"][4] and
+            cube["right"][8] != cube["right"][4]
+            ) or
+            (
+            cube["back"][6] != cube["bottom"][4] and
+            cube["back"][6] != cube["back"][4] and
+            cube["back"][6] != cube["right"][4]
+            ) or
+            # bottom left
+            (
+            cube["bottom"][6] != cube["bottom"][4] and
+            cube["bottom"][6] != cube["back"][4] and
+            cube["bottom"][6] != cube["left"][4]
+            ) or
+            (
+            cube["left"][6] != cube["bottom"][4] and
+            cube["left"][6] != cube["back"][4] and
+            cube["left"][6] != cube["left"][4]
+            ) or
+            (
+            cube["back"][8] != cube["bottom"][4] and
+            cube["back"][8] != cube["back"][4] and
+            cube["back"][8] != cube["left"][4]
+            )):
+        if ((cube["top"][2] != cube["top"][4] and
+                cube["top"][2] != cube["back"][4] and
+                cube["top"][2] != cube["right"][4]) or
+                (cube["right"][2] != cube["top"][4] and
+                cube["right"][2] != cube["back"][4] and
+                cube["right"][2] != cube["right"][4]) or
+                (cube["back"][0] != cube["top"][4] and
+                cube["back"][0] != cube["back"][4] and
+                cube["back"][0] != cube["right"][4])):
+            rotate_cube_clockwise(cube, solution)
+            i += 1
+        else:
+            third_layer_algorithm_position_corners(cube, solution)
+            i = 0
+        if i  == 4:
+            i = 0
+            third_layer_algorithm_position_corners(cube, solution)
+
 def complete_third_layer(cube, solution):
+    # print_cube(cube)
     complete_back_cross(cube, solution)
-    print_cube(cube)
+    print()
+    # print_cube(cube)
     match_back_edges_to_sides(cube, solution)
+    print()
+    print_cube(cube)
+    position_back_corners_to_sides(cube, solution)
+    print()
+    print_cube(cube)
 
 def main():
     cube = get_cube_from_pictures()
     solution  = []
-    print_cube(cube)
     complete_third_layer(cube, solution)
     print()
-    print_cube(cube)
     print(solution)
 
 main()
